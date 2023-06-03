99fdab5373509ac248deb66345c4b6686f441c27
khoa.77pm <khoa.77pm@gmail.com>
2023-06-03 11:29:08 +0700

    ng For add/remove items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,21 @@ import { Component, OnChanges, SimpleChanges } from '@angular/core';
 import { FavoriteChangeEventArg } from './favorites/favorites.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {  
+export class AppComponent {
+  removeCourse(course: { id: number; name: string }) {
+    let index = this.courses.indexOf(course);    
+    this.courses.splice(index,1);
+  }
+  addCourse() {
+    this.courses.push({id:10,name:"New course"})
+  }
   courses = [
-    {id:1,name:"Math"},
-    {id:2,name:"Physical"},
-    {id:3,name:"Biology"}
-  ]
+    { id: 1, name: "Math" },
+    { id: 2, name: "Physical" },
+    { id: 3, name: "Biology" },
+  ];
 }
